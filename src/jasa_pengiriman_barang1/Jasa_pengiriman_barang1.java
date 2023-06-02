5d4eb1fa0538cecee16b9446d6a56e66e51958db
naufalizzuddin <naufaltaufikh3@gmail.com>
2023-06-02 21:30:27 +0700

    upload

---
+++ src/jasa_pengiriman_barang1/Jasa_pengiriman_barang1.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package jasa_pengiriman_barang1;
+
+/**
+ *
+ * @author Naufal
+ */
+public class Jasa_pengiriman_barang1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        loginFrame buka = new loginFrame();
+        buka.setVisible(true);
+
+    }
+    
+}
